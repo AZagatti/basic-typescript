db2d4a30ea6bdd06b3a3e84e8119b71a7282fbee
azagatti <andre.zagatti1@gmail.com>
2020-03-07 14:44:52 -0300

    feat: Finished front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import User from './components/User';
+import api from './services/api';
+
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
-  return <div className='App'></div>;
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    api.get<IUser[]>('/users').then(response => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      {users.map(user => (
+        <User key={user.email} user={user} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
